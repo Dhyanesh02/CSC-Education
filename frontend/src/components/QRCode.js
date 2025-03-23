6fb6b9db498d0877f2cde9f7af580fa907f86991
Dhyanesh02 <dhyaneshcodespace@gmail.com>
2025-03-23 13:59:24 +0530

    QR scan Added

--- frontend/src/components/QRCode.js
+++ frontend/src/components/QRCode.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import QRCode from 'qrcode.react';
+import { QRCodeSVG } from 'qrcode.react';
 
 const AdmitCardQR = () => {
   const admitCardUrl = "https://iteducationjobs.com/sat2023/"; // Replace with your actual admit card URL
 
   return (
-    <QRCode
+    <QRCodeSVG
       value={admitCardUrl}
       size={180}
       level="H"
       includeMargin={true}
-      renderAs="svg"
       imageSettings={{
-        src: "/images/brochures/WhatsApp Image 2025-03-19 at 18.33.32_0ba4b9f4.jpg",
+        src: "/images/brochures",
         x: undefined,
         y: undefined,
         height: 24,
